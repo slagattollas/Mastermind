4dbe27e40546d498b3b95a400f5884b7f53587e1
Stefano <stefanolagattolla.s@gmail.com>
2020-10-17 14:03:20 +0200

    fix files

--- src/main/views/ResumeView.java
+++
@@ -1,9 +0,0 @@
-package com.mastermindstefano.main.views;
-
-import com.mastermindstefano.utils.Print;
-
-public class ResumeView {
-    public boolean isGameResumed(){
-        return Print.instance().readResume(Message.RETRY.toString());
-    }
-}
