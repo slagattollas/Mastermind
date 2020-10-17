4e36c2f145eab57dad54293cd7cb028fba522faf
Stefano <stefanolagattolla.s@gmail.com>
2020-10-17 13:38:57 +0200

    add graphics statements

---
+++ src/main/GraphicsMastermind.java
@@ -0,0 +1,12 @@
+package com.mastermindstefano.main;
+
+import com.mastermindstefano.main.models.Board;
+import com.mastermindstefano.main.views.graphics.View;
+
+public class GraphicsMastermind {
+    private Board board;
+    private View view;
+    GraphicsMastermind(){
+        this.view = new View();
+    }
+}
