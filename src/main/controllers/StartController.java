371e19ff91df063a715c3d0d6f6e6a6ea2cc2d57
Stefano <stefanolagattolla.s@gmail.com>
2020-10-27 18:21:34 +0100

    quitar reset en start

--- src/main/controllers/StartController.java
+++ src/main/controllers/StartController.java
@@ -12,7 +12,6 @@ public class StartController extends Controller implements AcceptorController {
         controllerVisitor.visit(this);
     }
     public void next(){
-        this.session.reset();
         this.session.next();
     }
 
